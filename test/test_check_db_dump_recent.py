a6c289c0077b0f2bc70912736ecceb4da189171c
gregcorbett <gregcorbett@users.noreply.github.com>
2023-08-25 14:45:10 +0100

    Add test case for non existant log file

--- test/test_check_db_dump_recent.py
+++ test/test_check_db_dump_recent.py
@@ -213,6 +213,19 @@ class TestCheckDBDumpRecent(unittest.TestCase):
 
         self._run_test(log_line_list, check.check_db_dump_recent.RETURN_CODE_CRITICAL)
 
+    def test_no_log(self):
+        """Test the check script given a non existent input."""
+        return_code = self.test_checker.run("/not/a/file")
+
+        self.assertEqual(
+            return_code,
+            check.check_db_dump_recent.RETURN_CODE_CRITICAL,
+            "Expected code %s, but got %s." % (
+                check.check_db_dump_recent.RETURN_CODE_CRITICAL,
+                return_code,
+            ),
+        )
+
     def _list_to_temp_file(self, input_list):
         """Take the given list and write it to self.temp_file_path."""
         with open(self.temp_file_path, "w") as temp_file:
